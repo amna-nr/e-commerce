ef211976fac9b4b28e1dbb0efc85124e2c19dba5
amna-nr <amnanoor5778@gmail.com>
2026-04-04 14:17:20 +0500

    add login endpoint

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -1,10 +1,13 @@
 # auth file routes and functions 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from core.database import db_dependency
 from schemas import UserRegister
 from starlette import status
 import bcrypt
 from models.models import User
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
+
 
 router = APIRouter(
     prefix="/auth",
@@ -19,8 +22,8 @@ def register(db: db_dependency, credentials: UserRegister):
     if credentials.password != credentials.confirm_password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Passwords don't match"
-            )
+            detail="Passwords don't match."
+        )
     
     # hash password 
     password_hash = bcrypt.hashpw(credentials.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
@@ -31,16 +34,33 @@ def register(db: db_dependency, credentials: UserRegister):
     db.commit()
     db.refresh(new_user)
 
-    return {"message": "User has been created"}
+    return {"message": "User has been created."}
+
 
 # login 
-# check if user exists in db 
-# hash password 
-# check password in db vs users both hashed 
-# if login successful generate access token 
-# generate refresh token 
-# store refresh token in redis 
-# return both tokens 
+@router.post("/login")
+def login(db: db_dependency, credentials: OAuth2PasswordRequestForm = Depends()):
+
+    # check if user exists in db 
+    user = db.query(User).filter(User.username == credentials.username).first()
+
+    if user is None: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+    
+    # check password in db vs users both hashed
+    if not bcrypt.checkpw(credentials.password.encode("utf-8"), bcrypt.gensalt()):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password."
+        )
+    
+    # generate access token 
+    # generate refresh token 
+    # store refresh token in redis 
+    # return both tokens 
 
 # refresh 
 # check if refresh token exists in redis 
@@ -49,4 +69,7 @@ def register(db: db_dependency, credentials: UserRegister):
 # invalidate old refresh token 
 # generate new refresh token 
 # return both tokens 
+
+# generate access token function 
+
 
