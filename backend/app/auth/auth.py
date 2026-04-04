d1dec05166a236de27d0c5332db595f899533f86
amna-nr <amnanoor5778@gmail.com>
2026-04-04 14:04:36 +0500

    add register endpoint

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -1,9 +1,37 @@
 # auth file routes and functions 
+from fastapi import APIRouter, HTTPException
+from core.database import db_dependency
+from schemas import UserRegister
+from starlette import status
+import bcrypt
+from models.models import User
 
-# register 
-# check if passwords match 
-# hash password 
-# store username and password in db 
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+# register
+@router.post("/register")
+def register(db: db_dependency, credentials: UserRegister):
+
+    # check if passwords match 
+    if credentials.password != credentials.confirm_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Passwords don't match"
+            )
+    
+    # hash password 
+    password_hash = bcrypt.hashpw(credentials.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+    # store username and password in db
+    new_user = User(username= credentials.username, password_hash=password_hash)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User has been created"}
 
 # login 
 # check if user exists in db 
