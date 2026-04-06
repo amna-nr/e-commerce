15a2c66f6e846ae70c7a7cbe6d9fd6664e58f758
amna-nr <amnanoor5778@gmail.com>
2026-04-06 13:27:33 +0500

    fix imports path in alembic env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,9 +11,9 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-from backend.app.core.database import Base
-from backend.app.core.config import settings
-import backend.app.models.models as models 
+from app.core.database import Base
+from app.core.config import settings
+import app.models.models as models 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
