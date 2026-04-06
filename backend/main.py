8aacebf299938ffd513c6bfa05661961a8fda728
amna-nr <amnanoor5778@gmail.com>
2026-04-06 12:19:14 +0500

    make product routes async and comment the code

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette import status 
 from contextlib import asynccontextmanager
 from app.core.redis import redis_client
+from sqlalchemy import select
 
 from app.auth.auth import get_current_user, router as auth_router
 from app.core.database import db_dependency
@@ -10,6 +11,7 @@ from app.models.models import User, Product
 from app.schemas.schemas import ProductIn, ProductOut, ProductUpdate
 
 
+# start redis on startup and close on shutdown
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await redis_client.ping()
@@ -18,8 +20,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# include auth routes
 app.include_router(auth_router)
 
+# enable frontend to access backend on the browser
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5173"],
@@ -28,53 +32,79 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def home():
     return {"message": "welcome"}
 
-@app.get("/products", response_model=list[ProductOut])
-def products_list(db: db_dependency, user: User = Depends(get_current_user)):
-    products = db.query(Product).all()
-    return products
 
+# show all products
+@app.get("/products", response_model=list[ProductOut])
+async def products_list(db: db_dependency, user: User = Depends(get_current_user)):
+
+    result = await db.execute(Product)
+    products = result.scalars().all()
+    return products
+    
+
+# create new product
 @app.post("/products", response_model=list[ProductOut])
 async def products_create(product: ProductIn, db: db_dependency, user: User = Depends(get_current_user)):
+
     new_product = Product(
         title = product.title,
         price = product.price, 
         stock = product.stock
     )
+
     db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
+    await db.commit()
+    await db.refresh(new_product)
+
     return new_product
 
+
+# update existing product
 @app.put("/products/{id}")
 async def products_update(id: int, product_update: ProductUpdate, db: db_dependency, user: User = Depends(get_current_user)):
 
-    product = db.query(Product).filter(Product.id == id).first()
+    # get product from db
+    result = await db.execute(select(Product).where(Product.id == id))
+    product = result.scalar_one_or_none()
 
+    # check if product exists
     if not product: 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product not found"
+            )
     
     for key, value in product_update.dict(exclude_unset=True).items():
         setattr(product, key, value)
 
-    db.commit()
-    db.refresh(product)
+    await db.commit()
+    await db.refresh(product)
 
     return product
 
 
+# delete product 
 @app.delete("/products/{id}")
 async def products_delete(id: int, db: db_dependency, user: User = Depends(get_current_user)):
 
-    product = db.query(Product).filter(Product.id == id).first()
+    # get product from db 
+    result = await db.execute(select(Product).where(Product.id == id))
+    product = result.scalar_one_or_none()
 
+    # check if product exists
     if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Product not found."
+            )
     
-    db.delete(product)
-    db.commit()
+    # delete the product 
+    await db.delete(product)
+    await db.commit()
 
     return {"message" : "Product has been deleted"}
